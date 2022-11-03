f4a3c2b6d769e1d3547d15e9d253195f30324704
lcgarner <113074918+lcgarner@users.noreply.github.com>
2022-11-02 23:54:26 -0400

    Add files via upload

--- House.java
+++ House.java
@@ -1,12 +1,92 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
+/*
+ * Represents a house.
+ */
+public class House extends Building{
+
+  private ArrayList<String> residents;
+  private Boolean hasDiningRoom;
+
+  /* Creates a house with the specifed parameters.
+  * @param name The house's name.
+  * @param address The house's first name.
+  * @param nFloors number of floors
+  * @param residents list of house residents 
+  * @param hDR if the house has a dining hall or not
+  */
+  public House(String name, String address, int nFloors, ArrayList<String> residents, Boolean hDR) {
+
+    super(name, address, nFloors);
+    this.residents = residents;
+    this.hasDiningRoom = hDR;
+    
     System.out.println("You have built a house: 🏠");
   }
+/*
+ * Finds the number of residents in the house.
+ * @return This houses number of residents.
+ */
+public int nResidents(){
+  return this.residents.size();
+}
+
+/*
+ * Checks if the house has a dining room.
+ * @return Boolean for whether house has a dining room.
+ */
+public boolean hasDiningRoom(){
+  return this.hasDiningRoom;
+}
+
+/*
+ * Removes resident from the house.
+ * @param residentName string residents name that moved out.
+ */
+public String moveOut(String residentName){
+  int indexOfResident = this.residents.indexOf(residentName);
+  this.residents.remove(indexOfResident);
+  return residentName;
+}
+
+/*
+ * Moves in resident.
+ * @param residentName string residents name that moved in.
+ */
+public void moveIn(String residentName){
+  this.residents.add(residentName);
+}
+
+/*
+ * Determines if person is resident of a house.
+ * @param person string person who is or isn't a resident
+ */
+public boolean isResident(String person){
+  return this.residents.contains(person);
+}
+
+/*
+ * Gives information about house
+ */
+public String toString(){
+  String diningHall;
+  if (hasDiningRoom() == true){
+    diningHall = "a dining Hall.";
+  } else {
+    diningHall = "no dining Hall.";
+  }
+  return this.name + " house is a " + this.nFloors + "-story building located at " + this.address + " with " + nResidents() + " residents and " + diningHall;
+}
 
   public static void main(String[] args) {
-    new House();
-  }
+    ArrayList<String> cushingResidents = new ArrayList<String>();
+    cushingResidents.add("Nathalie"); cushingResidents.add("Lara"); cushingResidents.add("Clare"); cushingResidents.add("Bena");
 
+    House cushing = new House("Cushing", "180 elm street", 4, cushingResidents, true);
+
+    System.out.println(cushing.toString());  
+
+    System.out.println(cushing.isResident("Lara"));
+    System.out.println(cushing.isResident("k"));
+  }
 }
--- reflection.md
+++ reflection.md
@@ -1,4 +1,8 @@
 Use this file to record your reflection on this assignment.
 
+***I worked with Liberty Lehr and Nathalie Trouba on this assignment.***
+
 - What are your initial impressions of how `Inheritance` affects your programming?
+    Building is like a parent class to the Cafe, House, and Library classes because these classes inherit the common traits of a Building while also having their own specific functionalities. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    Inheritance can be a little confusing at first, more syntactically than conceptually, but playing around with it helped. Creating instances of each class and testing them within main() was necessary for me while developing my code, and I would recommend for anyone taking this course in the future to do the same. 
